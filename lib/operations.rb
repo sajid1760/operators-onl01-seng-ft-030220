5587a66c98b5b763eda4ca322f1d1706c06abfa7
Sajid Anjum <sajid1760@gmail.com>
2020-03-03 20:24:18 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+if (speed > 60) || (speed < 40)
+  return TRUE
+else return FALSE
 end
-
-
+end
 
 def not_safe?(speed)
-	
+(speed > 60) || (speed < 40) ? return TRUE ; return FALSE
 end
-	
+
+
 
 
